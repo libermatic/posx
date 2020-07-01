65947ac2dd431657c78367db82a22bb08c0d7a69
Sun Howwrongbum <sun@libermatic.com>
2020-07-01 09:18:22 +0000

    feat: create or select batch by batch_price

--- posx/doc_events/purchase_receipt.py
+++ posx/doc_events/purchase_receipt.py
@@ -11,21 +11,16 @@ def before_validate(doc, method):
 
 def set_or_create_batch(doc, method):
     def set_existing_batch(item):
-        has_batch_no, has_expiry_date = frappe.get_cached_value(
-            "Item", item.item_code, ["has_batch_no", "has_expiry_date"]
-        )
-        if not has_batch_no or not has_expiry_date:
+        has_batch_no = frappe.get_cached_value("Item", item.item_code, "has_batch_no")
+        if not has_batch_no:
             return
 
-        if item.px_mfg_date:
-            item.batch_no = frappe.db.exists(
-                "Batch",
-                {"item": item.item_code, "manufacturing_date": item.px_mfg_date},
-            )
-        elif item.px_exp_date:
-            item.batch_no = frappe.db.exists(
-                "Batch", {"item": item.item_code, "expiry_date": item.px_exp_date},
-            )
+        item.batch_no = _get_existing(
+            item.item_code,
+            item.px_mfg_date,
+            item.px_exp_date,
+            item.px_batch_price_list_rate,
+        )
 
     get_batch_in_previous_items = compose(
         lambda x: x.get("batch_no"),
@@ -33,9 +28,8 @@ def set_or_create_batch(doc, method):
         lambda x: filter(
             lambda item: item.idx < x.idx
             and item.item_code == x.item_code
-            and (
-                item.px_mfg_date == x.px_mfg_date or item.px_exp_date == x.px_exp_date
-            ),
+            and (item.px_mfg_date == x.px_mfg_date or item.px_exp_date == x.px_exp_date)
+            and item.px_batch_price_list_rate == x.px_batch_price_list_rate,
             doc.items,
         ),
     )
@@ -44,12 +38,10 @@ def set_or_create_batch(doc, method):
         warehouse = "t_warehouse" if doc.doctype == "Stock Entry" else "warehouse"
         if not item.get(warehouse) or item.batch_no:
             return
-        has_batch_no, create_new_batch, has_expiry_date = frappe.get_cached_value(
-            "Item",
-            item.item_code,
-            ["has_batch_no", "create_new_batch", "has_expiry_date"],
+        has_batch_no, create_new_batch = frappe.get_cached_value(
+            "Item", item.item_code, ["has_batch_no", "create_new_batch"],
         )
-        if not has_batch_no or not create_new_batch or not has_expiry_date:
+        if not has_batch_no or not create_new_batch:
             return
 
         batch_in_items = get_batch_in_previous_items(item)
@@ -67,6 +59,7 @@ def set_or_create_batch(doc, method):
                         item.px_mfg_date, item.px_exp_date, shelf_life_in_days
                     ),
                     "expiry_date": item.px_exp_date,
+                    "px_price_list_rate": item.px_batch_price_list_rate,
                     "supplier": doc.supplier,
                     "reference_doctype": doc.doctype,
                     "reference_name": doc.name,
@@ -87,3 +80,29 @@ def _get_mfg_date(mfg_date, exp_date, shelf_life):
     if not mfg_date and exp_date and shelf_life:
         return frappe.utils.add_days(exp_date, -shelf_life)
     return mfg_date
+
+
+def _get_existing(item_code, manufacturing_date, expiry_date, px_price_list_rate):
+    if manufacturing_date:
+        return frappe.db.exists(
+            "Batch",
+            {
+                "item": item_code,
+                "manufacturing_date": manufacturing_date,
+                "px_price_list_rate": px_price_list_rate,
+            },
+        )
+    elif expiry_date:
+        return frappe.db.exists(
+            "Batch",
+            {
+                "item": item_code,
+                "expiry_date": expiry_date,
+                "px_price_list_rate": px_price_list_rate,
+            },
+        )
+    elif px_price_list_rate:
+        return frappe.db.exists(
+            "Batch", {"item": item_code, "px_price_list_rate": px_price_list_rate,},
+        )
+    return None
