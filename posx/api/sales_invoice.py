e46c180a39b341725bb86c04684707dadc468f3e
Sun Howwrongbum <sun@libermatic.com>
2020-07-03 10:56:05 +0000

    fix: batch_price not being fetched correctly

--- posx/api/sales_invoice.py
+++ posx/api/sales_invoice.py
@@ -19,11 +19,6 @@ def get_batch_price(batch_no, **kwargs):
             Currency
     """
     item_code = kwargs.get("item_code")
-    item_price_list_rate = get_price_list_rate_for(kwargs, item_code)
-    if not frappe.get_cached_value("Item", item_code, "px_has_batch_price"):
-        return item_price_list_rate
-
-    batch_price_list_rate = frappe.get_cached_value(
+    return frappe.get_cached_value(
         "Batch", batch_no, "px_price_list_rate"
-    )
-    return batch_price_list_rate or item_price_list_rate
+    ) or get_price_list_rate_for(kwargs, item_code)
