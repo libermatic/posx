cff8d41522cd459dbe14ed338e1b3ab40585058c
Sun Howwrongbum <sun@libermatic.com>
2020-07-03 10:55:06 +0000

    feat: do not show redundant batch selector

--- posx/public/js/pos/fixed_batch_selection.js
+++ posx/public/js/pos/fixed_batch_selection.js
@@ -4,6 +4,34 @@ export default function fixed_batch_selection(Pos) {
   return makeExtension(
     'fixed_batch_selection',
     class PosWithFixedBatchSelection extends Pos {
+      update_item_in_cart(item_code, field = 'qty', value = 1, batch_no) {
+        if (field === 'qty' && this.cart.exists(item_code, batch_no)) {
+          const item = this.frm.doc.items.find((x) =>
+            batch_no ? x.batch_no === batch_no : x.item_code === item_code
+          );
+
+          const updated_qty = item[field] + flt(value);
+          const show_dialog = this._should_show_sb_selector(item, updated_qty);
+
+          frappe.flags.hide_serial_batch_dialog = false;
+
+          if (show_dialog) {
+            frappe.dom.freeze();
+            this.select_batch_and_serial_no(item);
+          } else {
+            this.update_item_in_frm(item, field, updated_qty).then(() => {
+              frappe.dom.unfreeze();
+              frappe.run_serially([
+                () => this.on_qty_change(item),
+                () => this.post_qty_change(item),
+              ]);
+            });
+          }
+        } else {
+          super.update_item_in_cart(item_code, field, value, batch_no);
+        }
+      }
+
       async update_item_in_frm(item, field, value) {
         const { batch_no } = item;
         const result = await super.update_item_in_frm(item, field, value);
@@ -24,6 +52,33 @@ export default function fixed_batch_selection(Pos) {
           this.frm.doc.items.map(this.cart.get_item_html.bind(this.cart))
         );
         return result;
+      }
+
+      _should_show_sb_selector(item, update_qty) {
+        if (item.has_serial_no) {
+          return true;
+        }
+        if (item.has_batch_no) {
+          if (!item.batch_no) {
+            return true;
+          }
+
+          // will always show if multiple batches exists in cart
+          if (
+            this.frm.doc.items.filter((x) => x.item_code == item.item_code)
+              .length > 1
+          ) {
+            return true;
+          }
+
+          // upstream: will not show if actual_qty = actual_batch_qty
+          // here: not show selector until the updated_qty is greater than the
+          // actual_batch_qty
+          if (item.actual_batch_qty < update_qty) {
+            return true;
+          }
+        }
+        return false;
       }
     }
   );
