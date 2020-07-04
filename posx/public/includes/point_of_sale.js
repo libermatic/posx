4c649387b7c868d34a5dfb86170c55887d32b91a
Sun Howwrongbum <sun@libermatic.com>
2020-07-04 08:50:43 +0000

    fix: apply batch selector extension onn pos only

--- posx/public/includes/point_of_sale.js
+++ posx/public/includes/point_of_sale.js
@@ -1,2 +1,5 @@
 erpnext.pos.PointOfSale = posx.extensions.pos.page(erpnext.pos.PointOfSale);
 Payment = posx.extensions.pos.payment(Payment);
+
+erpnext.show_serial_batch_selector =
+  posx.extensions.serial_no_batch_selector.show_serial_batch_selector;
