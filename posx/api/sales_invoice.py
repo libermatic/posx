a550c51896e145f54a72459259503f689110b95d
Sun Howwrongbum <sun@libermatic.com>
2020-07-15 15:32:43 +0000

    feat: batch_price considers variants

--- posx/api/sales_invoice.py
+++ posx/api/sales_invoice.py
@@ -19,7 +19,17 @@ def get_batch_price(batch_no, **kwargs):
         Returns
             Currency
     """
+    batch_price = frappe.get_cached_value("Batch", batch_no, "px_price_list_rate")
+    if batch_price:
+        return batch_price
+
     item_code = kwargs.get("item_code")
-    return frappe.get_cached_value(
-        "Batch", batch_no, "px_price_list_rate"
-    ) or get_price_list_rate_for(kwargs, item_code)
+    item_price = get_price_list_rate_for(kwargs, item_code)
+    if item_price:
+        return item_price
+
+    template = frappe.get_cached_value("Item", item_code, "variant_of")
+    if template:
+        return get_price_list_rate_for(kwargs, template)
+
+    return None
