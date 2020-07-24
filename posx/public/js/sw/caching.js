6e8c8d4d2c670e849b3881d5603f7b2af1df8bcb
Sun Howwrongbum <sun@libermatic.com>
2020-07-24 21:32:09 +0000

    fix: do not use _ timestamp params in cache key

--- posx/public/js/sw/caching.js
+++ posx/public/js/sw/caching.js
@@ -1,10 +1,19 @@
 const SCRIPT_CACHE = 'posx-scripts-v1';
 
+const cacheKeyPlugin = {
+  cacheKeyWillBeUsed: async ({ request }) => {
+    const url = new URL(request.url);
+    url.searchParams.delete('_');
+    return url.toString();
+  },
+};
+
 workbox.routing.registerRoute(
-  ({ url }) => url.pathname === '/api/method/frappe.client.get_js',
+  ({ url }) => url.pathname.endsWith('frappe.client.get_js'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: SCRIPT_CACHE,
     plugins: [
+      cacheKeyPlugin,
       new workbox.expiration.ExpirationPlugin({
         maxEntries: 60,
         maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
