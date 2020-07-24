b4aa8109c68acf8cf715d4664edfa492407d782f
Sun Howwrongbum <sun@libermatic.com>
2020-07-24 21:32:36 +0000

    feat: cache getdoctype

--- posx/public/js/sw/caching.js
+++ posx/public/js/sw/caching.js
@@ -1,4 +1,5 @@
 const SCRIPT_CACHE = 'posx-scripts-v1';
+const DOCTYPE_CACHE = 'posx-doctype-v1';
 
 const cacheKeyPlugin = {
   cacheKeyWillBeUsed: async ({ request }) => {
@@ -12,6 +13,20 @@ workbox.routing.registerRoute(
   ({ url }) => url.pathname.endsWith('frappe.client.get_js'),
   new workbox.strategies.StaleWhileRevalidate({
     cacheName: SCRIPT_CACHE,
+    plugins: [
+      cacheKeyPlugin,
+      new workbox.expiration.ExpirationPlugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  ({ url }) => url.pathname.endsWith('frappe.desk.form.load.getdoctype'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: DOCTYPE_CACHE,
     plugins: [
       cacheKeyPlugin,
       new workbox.expiration.ExpirationPlugin({
