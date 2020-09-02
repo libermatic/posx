468300a3c266362d74df4b9e654a272dc4ba1a50
Sun Howwrongbum <sun@libermatic.com>
2020-09-02 18:23:54 +0000

    fix: db schema

--- posx/public/js/store/db.js
+++ posx/public/js/store/db.js
@@ -19,7 +19,7 @@ function getModelSchema() {
 
 const db = new Dexie('posx');
 
-db.version(1).stores({
+db.version(2).stores({
   session_state: 'key',
   settings: 'doctype',
   draft_invoices: 'offline_pos_name',
