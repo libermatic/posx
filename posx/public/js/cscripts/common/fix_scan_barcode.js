808e94564c8ed9cc62050c2895ca9c0dea4b17c5
Sun Howwrongbum <sun@libermatic.com>
2020-09-06 16:21:18 +0000

    fix(pos): scan_barcode handler

--- posx/public/js/cscripts/common/fix_scan_barcode.js
+++ posx/public/js/cscripts/common/fix_scan_barcode.js
@@ -8,7 +8,7 @@ export default function fix_scan_barcode(frm) {
     if (e.keyCode === 9 || e.keyCode === 13) {
       frm
         .set_value('scan_barcode', e.target.value)
-        .then(frm.trigger('scan_barcode'));
+        .then(() => frm.trigger('scan_barcode'));
       e.preventDefault();
       return false;
     }
