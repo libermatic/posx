5592371a1e4ed10e0b2023e33aa8c8d65e53c88e
Sun Howwrongbum <sun@libermatic.com>
2020-11-05 14:06:37 +0000

    refactor: move get_pricing_rule_items to utils

--- posx/public/js/store/server_methods/pricing_rule/pricing_rule.js
+++ posx/public/js/store/server_methods/pricing_rule/pricing_rule.js
@@ -7,6 +7,7 @@ import {
   ValidationError,
   UnsupportedFeatureError,
 } from '../../../utils/exceptions.js';
+import { get_pricing_rule_items, snakeCase } from './utils';
 
 export async function erpnext__accounts__doctype__pricing_rule__pricing_rule__apply_pricing_rule({
   args: _args,
@@ -322,34 +323,6 @@ async function get_pricing_rules(
 
 function apply_multiple_pricing_rules(rules) {
   return rules.every((x) => x.apply_multiple_pricing_rules);
-}
-
-// https://github.com/frappe/erpnext/blob/f7f8f5c305aa9481c9b142245eadb1b67eaebb9a/erpnext/accounts/doctype/pricing_rule/utils.py#L536
-async function get_pricing_rule_items(pr_doc) {
-  let apply_on_data = [];
-  const apply_on = snakeCase(pr_doc.apply_on);
-
-  const children = await db
-    .table(`Pricing Rule ${pr_doc.apply_on}`)
-    .where('parent')
-    .equals(pr_doc.name)
-    .toArray();
-
-  for (let row of children) {
-    if (apply_on === 'item_group') {
-      const descendents = await getDescendents('Item Group', row[apply_on]);
-      apply_on_data = [...apply_on_data, ...descendents];
-    } else {
-      apply_on_data = [...apply_on_data, row[apply_on]];
-    }
-  }
-
-  if (pr_doc.apply_rule_on_other) {
-    const other_apply_on = snakeCase(pr_doc.apply_rule_on_other);
-    apply_on_data = [...apply_on_data, pr_doc[`other_${other_apply_on}`]];
-  }
-
-  return Array.from(new Set(apply_on_data));
 }
 
 async function filter_pricing_rules(
@@ -678,15 +651,6 @@ async function getAncestors(doctype, name) {
     .then(R.map(R.prop('name')));
 }
 
-async function getDescendents(doctype, name) {
-  const { lft, rgt } = await db.table(doctype).get(name);
-  return db
-    .table(doctype)
-    .filter((x) => x.lft >= lft && x.rgt <= rgt)
-    .toArray()
-    .then(R.map(R.prop('name')));
-}
-
 async function getParents(doctype, value) {
   function query(allowed) {
     const apply_on = snakeCase(doctype);
@@ -718,8 +682,4 @@ async function getParents(doctype, value) {
     return query(allowed);
   }
   return [];
-}
-
-function snakeCase(text) {
-  return text.toLowerCase().replace(' ', '_');
 }
---
+++ posx/public/js/store/server_methods/pricing_rule/utils.js
@@ -0,0 +1,42 @@
+import db from '../../db';
+
+export function snakeCase(text) {
+  return text.toLowerCase().replace(' ', '_');
+}
+
+// https://github.com/frappe/erpnext/blob/f7f8f5c305aa9481c9b142245eadb1b67eaebb9a/erpnext/accounts/doctype/pricing_rule/utils.py#L536
+export async function get_pricing_rule_items(pr_doc) {
+  let apply_on_data = [];
+  const apply_on = snakeCase(pr_doc.apply_on);
+
+  const children = await db
+    .table(`Pricing Rule ${pr_doc.apply_on}`)
+    .where('parent')
+    .equals(pr_doc.name)
+    .toArray();
+
+  for (let row of children) {
+    if (apply_on === 'item_group') {
+      const descendents = await getDescendents('Item Group', row[apply_on]);
+      apply_on_data = [...apply_on_data, ...descendents];
+    } else {
+      apply_on_data = [...apply_on_data, row[apply_on]];
+    }
+  }
+
+  if (pr_doc.apply_rule_on_other) {
+    const other_apply_on = snakeCase(pr_doc.apply_rule_on_other);
+    apply_on_data = [...apply_on_data, pr_doc[`other_${other_apply_on}`]];
+  }
+
+  return Array.from(new Set(apply_on_data));
+}
+
+async function getDescendents(doctype, name) {
+  const { lft, rgt } = await db.table(doctype).get(name);
+  return db
+    .table(doctype)
+    .filter((x) => x.lft >= lft && x.rgt <= rgt)
+    .toArray()
+    .then(R.map(R.prop('name')));
+}
