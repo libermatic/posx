de97b000e89920197161f4c8cc371a8e6bf8f7c0
Sun Howwrongbum <sun@libermatic.com>
2020-11-10 21:01:28 +0000

    0.3.1

--- posx/public/js/index.js
+++ posx/public/js/index.js
@@ -9,7 +9,7 @@ function get_doctype(import_name) {
     .join(' ');
 }
 
-const __version__ = '0.3.0';
+const __version__ = '0.3.1';
 
 frappe.provide('posx');
 posx = { __version__, scripts, extensions };
