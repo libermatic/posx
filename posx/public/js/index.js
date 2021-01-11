4ed69e4e98c708b64a7dbf7d90dd35389f77486c
Sun Howwrongbum <sun@libermatic.com>
2021-01-11 18:32:47 +0000

    0.4.2

--- posx/public/js/index.js
+++ posx/public/js/index.js
@@ -9,7 +9,7 @@ function get_doctype(import_name) {
     .join(' ');
 }
 
-const __version__ = '0.4.1';
+const __version__ = '0.4.2';
 
 frappe.provide('posx');
 posx = { __version__, scripts, extensions };
