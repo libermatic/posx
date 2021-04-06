cb2f92733998cf46ab2cb392d9dc0cc1127baf9a
Sun Howwrongbum <sun@libermatic.com>
2021-04-06 17:14:19 +0000

    fix: use upstream batch price feature

--- posx/doc_events/purchase_receipt.py
+++ posx/doc_events/purchase_receipt.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 import frappe
+import erpnext
+from erpnext.stock.get_item_details import get_item_price
 from toolz.curried import compose, excepts, first, map, filter
 
 
@@ -10,6 +12,8 @@ def before_validate(doc, method):
 
 
 def set_or_create_batch(doc, method):
+    price_list = frappe.db.get_single_value("Selling Settings", "selling_price_list")
+
     def set_existing_batch(item):
         has_batch_no = frappe.get_cached_value("Item", item.item_code, "has_batch_no")
         if not has_batch_no:
@@ -17,6 +21,7 @@ def set_or_create_batch(doc, method):
 
         item.batch_no = _get_existing(
             item.item_code,
+            price_list,
             item.px_mfg_date,
             item.px_exp_date,
             item.px_batch_price_list_rate,
@@ -39,7 +44,9 @@ def set_or_create_batch(doc, method):
         if not item.get(warehouse) or item.batch_no:
             return
         has_batch_no, create_new_batch = frappe.get_cached_value(
-            "Item", item.item_code, ["has_batch_no", "create_new_batch"],
+            "Item",
+            item.item_code,
+            ["has_batch_no", "create_new_batch"],
         )
         if not has_batch_no or not create_new_batch:
             return
@@ -49,7 +56,9 @@ def set_or_create_batch(doc, method):
             item.batch_no = batch_in_items
         else:
             shelf_life_in_days = frappe.get_cached_value(
-                "Item", item.item_code, "shelf_life_in_days",
+                "Item",
+                item.item_code,
+                "shelf_life_in_days",
             )
             batch = frappe.get_doc(
                 {
@@ -59,13 +68,20 @@ def set_or_create_batch(doc, method):
                         item.px_mfg_date, item.px_exp_date, shelf_life_in_days
                     ),
                     "expiry_date": item.px_exp_date,
-                    "px_price_list_rate": item.px_batch_price_list_rate,
                     "supplier": doc.supplier,
                     "reference_doctype": doc.doctype,
                     "reference_name": doc.name,
                 }
             ).insert()
             item.batch_no = batch.name
+            if item.px_batch_price_list_rate:
+                _create_batch_price(
+                    price_list,
+                    doc.posting_date,
+                    item.item_code,
+                    item.px_batch_price_list_rate,
+                    batch.name,
+                )
 
     for item in doc.items:
         if not item.batch_no:
@@ -82,7 +98,9 @@ def _get_mfg_date(mfg_date, exp_date, shelf_life):
     return mfg_date
 
 
-def _get_existing(item_code, manufacturing_date, expiry_date, px_price_list_rate):
+def _get_existing(
+    item_code, price_list, manufacturing_date, expiry_date, px_price_list_rate
+):
     if manufacturing_date:
         return frappe.db.exists(
             "Batch",
@@ -102,7 +120,43 @@ def _get_existing(item_code, manufacturing_date, expiry_date, px_price_list_rate
             },
         )
     elif px_price_list_rate:
-        return frappe.db.exists(
-            "Batch", {"item": item_code, "px_price_list_rate": px_price_list_rate,},
+        item_prices = frappe.get_all(
+            "Item Price",
+            filters={
+                "item_code": item_code,
+                "price_list": price_list,
+                "price_list_rate": px_price_list_rate,
+                "batch_no": ("is", "set"),
+            },
+            fields=["batch_no"],
+            as_list=1,
         )
+        if item_prices:
+            return item_prices[0][0]
+
     return None
+
+
+def _create_batch_price(price_list, posting_date, item_code, price_list_rate, batch_no):
+    existing_price = get_item_price(
+        {
+            "posting_date": posting_date,
+            "price_list": price_list,
+            "uom": "",
+            "batch_no": "",
+        },
+        item_code,
+    )
+    if existing_price and existing_price[0][1] == price_list_rate:
+        return
+
+    frappe.get_doc(
+        {
+            "doctype": "Item Price",
+            "price_list": price_list,
+            "item_code": item_code,
+            "currency": erpnext.get_default_currency(),
+            "price_list_rate": price_list_rate,
+            "batch_no": batch_no,
+        }
+    ).insert()
