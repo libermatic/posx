eee3ce1c641defa2baae9ae4d79a3aadaac99f7d
Sun Howwrongbum <sun@libermatic.com>
2021-04-06 17:50:29 +0000

    fix: patch to migrate_to_upstream_batch_price

---
+++ posx/patches/v13_0/migrate_to_upstream_batch_price.py
@@ -0,0 +1,25 @@
+from __future__ import unicode_literals
+import frappe
+from erpnext.stock.get_item_details import get_item_price
+from posx.doc_events.purchase_receipt import _create_batch_price
+
+
+def execute():
+    batches_with_price = frappe.get_all(
+        "Batch",
+        filters={"px_price_list_rate": (">", 0)},
+        fields=["name", "item", "px_price_list_rate"],
+        as_list=1,
+    )
+
+    price_list = frappe.db.get_single_value(
+        "Selling Settings", "selling_price_list"
+    ) or frappe.db.get_value("Price List", frappe._("Standard Selling"))
+    posting_date = frappe.utils.today()
+
+    for batch_no, item_code, price_list_rate in batches_with_price:
+        _create_batch_price(
+            price_list, posting_date, item_code, price_list_rate, batch_no
+        )
+
+    frappe.delete_doc_if_exists("Custom Field", "Batch-px_price_list_rate")
