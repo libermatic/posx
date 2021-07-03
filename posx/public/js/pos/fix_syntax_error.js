289ccf83258255318fe399d30d0c635dfbf77699
Sun Howwrongbum <sun@libermatic.com>
2021-07-03 14:16:51 +0000

    fix(pos): migrate fix to function signature

--- posx/public/js/pos/fix_syntax_error.js
+++ posx/public/js/pos/fix_syntax_error.js
@@ -1,5 +1,8 @@
 import { makeExtension } from '../utils';
 
+/**
+ * remove item from cart view and close item detail when qty is set to 0
+ */
 export default function fix_syntax_error(Controller) {
   return makeExtension(
     'fix_syntax_error',
@@ -7,25 +10,19 @@ export default function fix_syntax_error(Controller) {
       init_item_details() {
         super.init_item_details();
         this.item_details.events.form_updated = async function (
-          cdt,
-          cdn,
-          fieldname,
+          item,
+          field,
           value
         ) {
-          const item_row = frappe.model.get_doc(cdt, cdn);
-          if (item_row && item_row[fieldname] != value) {
-            if (fieldname === 'qty' && flt(value) == 0) {
+          const item_row = frappe.model.get_doc(item.doctype, item.name);
+          if (item_row && item_row[field] != value) {
+            if (field === 'qty' && flt(value) == 0) {
               this.remove_item_from_cart();
               return;
             }
 
-            const { item_code, batch_no, uom } = this.item_details.current_item;
-            const event = {
-              field: fieldname,
-              value,
-              item: { item_code, batch_no, uom },
-            };
-            return this.on_cart_update(event);
+            const args = { field, value, item: this.item_details.current_item };
+            return this.on_cart_update(args);
           }
         }.bind(this);
       }
