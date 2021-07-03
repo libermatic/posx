0cf598ba1a66678de1488872ceca2da16c65e1f2
Sun Howwrongbum <sun@libermatic.com>
2021-07-03 19:18:36 +0000

    dix: remove unused

--- posx/public/js/pos/set_batch_number.js
+++ posx/public/js/pos/set_batch_number.js
@@ -4,15 +4,6 @@ export default function set_batch_number(Controller) {
   return makeExtension(
     'set_batch_number',
     class ControllerWithSetBatchNumber extends Controller {
-      prepare_components() {
-        super.prepare_components();
-        frappe.ui.form.on('POS Invoice Item', 'rate', (frm, cdt, cdn) => {
-          const doc = frappe.model.get_doc(cdt, cdn);
-          if (this.cart.get_cart_item(doc).length > 0) {
-            this.cart.update_item_html(doc);
-          }
-        });
-      }
       async trigger_new_item_events(item_row) {
         item_row.warehouse = this.frm.doc.set_warehouse;
         await super.trigger_new_item_events(item_row);
