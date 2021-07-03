2dbef092594e3720251c42d5f6524873415653a9
Sun Howwrongbum <sun@libermatic.com>
2021-07-03 14:16:20 +0000

    fix(pos): cart amount not being updated

--- posx/public/js/pos/index.js
+++ posx/public/js/pos/index.js
@@ -1,8 +1,9 @@
 import discount_amount from './discount_amount';
 import set_batch_number from './set_batch_number';
 import fix_syntax_error from './fix_syntax_error';
+import fix_late_cart_amount_update from './fix_late_cart_amount_update';
 import rerender_fields from './rerender_fields';
 
 export const controllerOverrides = [set_batch_number, fix_syntax_error];
-export const cartOverrides = [discount_amount];
+export const cartOverrides = [discount_amount, fix_late_cart_amount_update];
 export const detailsOverrides = [rerender_fields];
