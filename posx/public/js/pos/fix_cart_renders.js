6c7467e67f15cc290c82726b27debd7b28e50df3
Sun Howwrongbum <sun@libermatic.com>
2021-07-06 21:42:01 +0000

    fix(pos): grand total not rendering

--- posx/public/js/pos/fix_cart_renders.js
+++ posx/public/js/pos/fix_cart_renders.js
@@ -2,6 +2,8 @@ import { makeExtension, waitToChange } from '../utils';
 
 /**
  * hack to fix various renders when model changes
+ * this is caused by #25961. when _calculate_taxes_and_totals changed to run as async
+ * should remove this when race issues get resolved
  */
 export default function fix_cart_renders(ItemCart) {
   return makeExtension(
@@ -9,10 +11,21 @@ export default function fix_cart_renders(ItemCart) {
     class ItemCartFixCartRenders extends ItemCart {
       bind_events() {
         super.bind_events();
+
+        frappe.model.on('POS Invoice', '*', (fieldname, value, doc) => {
+          if (fieldname === 'discount_amount') {
+            waitToChange('grand_total', this.events.get_frm().doc, 1000).then(
+              () => {
+                this.update_totals_section();
+              }
+            );
+          }
+        });
+
         frappe.model.on('POS Invoice Item', '*', (fieldname, value, doc) => {
           if (['rate', 'qty'].includes(fieldname)) {
             // totals not rendering when ItemDetails fields are updated
-            waitToChange('amount', doc, 5000).then(() => {
+            waitToChange('amount', doc, 1000).then(() => {
               if (this.get_item_from_frm(doc)) {
                 this.update_item_html(doc);
               }
@@ -20,7 +33,7 @@ export default function fix_cart_renders(ItemCart) {
 
             // item cart amount render not following model update
             if (fieldname === 'rate') {
-              waitToChange('grand_total', this.events.get_frm().doc, 5000).then(
+              waitToChange('grand_total', this.events.get_frm().doc, 1000).then(
                 () => {
                   this.update_totals_section();
                 }
