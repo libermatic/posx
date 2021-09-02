db230adb2c2f32f1fd66c718879838b7531a4175
Sun Howwrongbum <sun@libermatic.com>
2021-09-02 17:49:24 +0000

    fix: tax render error on new invoice

--- posx/public/js/pos/fix_cart_renders.js
+++ posx/public/js/pos/fix_cart_renders.js
@@ -1,11 +1,15 @@
 import { makeExtension, waitToChange } from '../utils';
 
 /**
- *
+ * fixes undefined `taxes` prop in frm.doc on new invoice
  */
 export default function fix_cart_renders(ItemCart) {
   return makeExtension(
     'fix_cart_renders',
-    class ItemCartFixCartRenders extends ItemCart {}
+    class ItemCartFixCartRenders extends ItemCart {
+      render_taxes(taxes) {
+        super.render_taxes(taxes || []);
+      }
+    }
   );
 }
