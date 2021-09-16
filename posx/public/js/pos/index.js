fc24ebaedac26851a808281c37aff0d6aee50330
Sun Howwrongbum <sun@libermatic.com>
2021-09-16 11:30:19 +0000

    feat: remove fix_payment_method_select
    superceded by https://github.com/frappe/erpnext/pull/27409 and https://github.com/frappe/erpnext/pull/27251

--- posx/public/js/pos/index.js
+++ posx/public/js/pos/index.js
@@ -2,10 +2,9 @@ import discount_amount from './discount_amount';
 import set_batch_number from './set_batch_number';
 import fix_remove_row_on_zero from './fix_remove_row_on_zero';
 import fix_cart_renders from './fix_cart_renders';
-import fix_payment_method_select from './fix_payment_method_select';
 import allow_zero_payment from './allow_zero_payment';
 import ignore_availability from './ignore_availability';
 
 export const controllerOverrides = [ignore_availability, set_batch_number, fix_remove_row_on_zero];
 export const cartOverrides = [discount_amount, fix_cart_renders];
-export const paymentOverrides = [allow_zero_payment, fix_payment_method_select];
+export const paymentOverrides = [allow_zero_payment];
