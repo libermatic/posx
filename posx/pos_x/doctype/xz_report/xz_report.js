2c87d2d436f2867fb2e9fe78d9042518e99980a5
Sun Howwrongbum <sun@libermatic.com>
2022-09-23 22:10:36 +0000

    fix: remove XZReport

--- posx/pos_x/doctype/xz_report/xz_report.js
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2020, Libermatic and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on("XZ Report", posx.scripts.xz_report());
--- posx/public/js/scripts/index.js
+++ posx/public/js/scripts/index.js
@@ -1,2 +1,1 @@
-export { default as xz_report } from './xz_report';
 export * from './label_printer';
