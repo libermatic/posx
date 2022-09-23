792dea30da2697cac66258ad2680797487adf21c
Sun Howwrongbum <sun@libermatic.com>
2022-09-23 22:20:53 +0000

    feat: POSAsset

---
+++ pos_asset/pos_asset.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, Libermatic and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('POS Asset', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pos_asset/test_pos_asset.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, Libermatic and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestPOSAsset(unittest.TestCase):
+	pass
