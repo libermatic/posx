ec412eb568ccc77932caebfb0f2dd4dfc36998a4
Sun Howwrongbum <sun@libermatic.com>
2022-09-23 22:05:11 +0000

    fix: batch ItemPrice auto creation

--- posx/doc_events/purchase_receipt.py
+++ posx/doc_events/purchase_receipt.py
@@ -43,6 +43,7 @@ def set_or_create_batch(doc, method):
         warehouse = "t_warehouse" if doc.doctype == "Stock Entry" else "warehouse"
         if not item.get(warehouse) or item.batch_no:
             return
+
         has_batch_no, create_new_batch = frappe.get_cached_value(
             "Item",
             item.item_code,
@@ -74,22 +75,26 @@ def set_or_create_batch(doc, method):
                 }
             ).insert()
             item.batch_no = batch.name
+
+    for item in doc.items:
+        if not item.batch_no:
+            set_existing_batch(item)
+
+    for item in doc.items:
+        warehouse = "t_warehouse" if doc.doctype == "Stock Entry" else "warehouse"
+        if item.get(warehouse) and not item.batch_no:
+            create_new_batch(item)
+    
+    for item in doc.items:
+        if item.batch_no and item.px_batch_price_list_rate:
             if item.px_batch_price_list_rate:
                 _create_batch_price(
                     price_list,
                     doc.posting_date,
                     item.item_code,
                     item.px_batch_price_list_rate,
-                    batch.name,
+                    item.batch_no,
                 )
-
-    for item in doc.items:
-        if not item.batch_no:
-            set_existing_batch(item)
-    for item in doc.items:
-        warehouse = "t_warehouse" if doc.doctype == "Stock Entry" else "warehouse"
-        if item.get(warehouse) and not item.batch_no:
-            create_new_batch(item)
 
 
 def _get_mfg_date(mfg_date, exp_date, shelf_life):
@@ -99,55 +104,62 @@ def _get_mfg_date(mfg_date, exp_date, shelf_life):
 
 
 def _get_existing(
-    item_code, price_list, manufacturing_date, expiry_date, px_price_list_rate
+    item_code, price_list, manufacturing_date, expiry_date, price_list_rate
 ):
+    has_batch_details = bool(manufacturing_date) or bool(expiry_date)
+    has_price = bool(price_list_rate)
+
+    batch_filters = {"item": item_code}
+
     if manufacturing_date:
-        return frappe.db.exists(
-            "Batch",
-            {
-                "item": item_code,
-                "manufacturing_date": manufacturing_date,
-                "px_price_list_rate": px_price_list_rate,
-            },
-        )
-    elif expiry_date:
-        return frappe.db.exists(
-            "Batch",
-            {
-                "item": item_code,
-                "expiry_date": expiry_date,
-                "px_price_list_rate": px_price_list_rate,
-            },
-        )
-    elif px_price_list_rate:
-        item_prices = frappe.get_all(
+        batch_filters.update({"manufacturing_date": manufacturing_date})
+    if expiry_date:
+        batch_filters.update({"expiry_date": expiry_date})
+
+    batches = frappe.get_all("Batch", filters=batch_filters)
+    prices = (
+        frappe.get_all(
             "Item Price",
             filters={
                 "item_code": item_code,
                 "price_list": price_list,
-                "price_list_rate": px_price_list_rate,
+                "price_list_rate": price_list_rate,
                 "batch_no": ("is", "set"),
             },
             fields=["batch_no"],
-            as_list=1,
         )
-        if item_prices:
-            return item_prices[0][0]
+        if price_list_rate
+        else []
+    )
+
+    for price in prices:
+        for batch in batches:
+            if batch.get("name") == price.get("batch_no"):
+                return batch["name"]
+
+    if not has_price and batches:
+        return batches[0]["name"]
+
+    if not has_batch_details and prices:
+        return prices[0]["batch_no"]
 
     return None
 
 
 def _create_batch_price(price_list, posting_date, item_code, price_list_rate, batch_no):
-    existing_price = get_item_price(
-        {
-            "posting_date": posting_date,
-            "price_list": price_list,
-            "uom": "",
-            "batch_no": "",
-        },
-        item_code,
-    )
-    if existing_price and existing_price[0][1] == price_list_rate:
+    def has_existing_price(batch: str):
+        existing_price = get_item_price(
+            {
+                "transaction_date": posting_date,
+                "price_list": price_list,
+                "uom": "",
+                "batch_no": batch,
+            },
+            item_code,
+        )
+        return bool(existing_price) and existing_price[0][1] == price_list_rate
+
+    if has_existing_price("") or has_existing_price(batch_no):
         return
 
     frappe.get_doc(
