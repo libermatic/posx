ff0801b7bdcd2efecdf9fec8b55c650f6d39ed59
Sun Howwrongbum <sun@libermatic.com>
2022-09-23 22:11:29 +0000

    fix: remove POSXSettings

--- posx/pos_x/doctype/pos_x_settings/pos_x_settings.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2020, Libermatic and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('POS X Settings', {
-	// refresh: function(frm) {
-
-	// }
-});
